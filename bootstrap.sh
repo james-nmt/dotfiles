9bda3f96eab3d63be39612f4f0aeb82211e453ae
Nguyen Minh Trung <James@Nguyens-MacBook-Air.local>
2014-05-05 20:53:43 +0800

    .slate.js included in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 	source ~/.bash_profile
+  cp .slate.js ~/
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
